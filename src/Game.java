1cbd24af9532ec7a8f117e210a774c216b448a75
AnGoga <Anogeorgy05@gmail.com>
2020-12-01 21:40:54 +0300

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+
+public class Game{
+    public void start(){
+        HelloFunction();
+    }
+
+    private void HelloFunction() {
+        out.println("Добро пожаловать в симулятор ");
+    }
+}
